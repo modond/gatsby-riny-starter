abf1cfdcc1e70d4fe5f0faf71a91c4a4632bb3b3
Monique de Haas <monique.dondersteen@gmail.com>
2019-07-10 17:26:22 +0200

    Update index.js
    added telephone link, deleted blogposts, more space for button

--- src/pages/index.js
+++ src/pages/index.js
@@ -53,32 +53,20 @@ const IndexPage = ({
         <p>Na tientallen jaren gewerkt te hebben met diverse presentatoren, artiesten,
 CEO, ministers, producenten en eindredacteuren heb ik mijn opgedane
 kennis in een eigenwijze vorm gegoten.</p>
-        <Link to="/contact">
+        <Link to="tel:+31653960521">bel ons direct</a>>
           <Button big>
             <svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
               <path d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45-14 8-31 8-11 0-24-5l-453-185-242 295q-18 23-49 23-13 0-22-4-19-7-30.5-23.5t-11.5-36.5v-349l864-1059-1069 925-395-162q-37-14-40-55-2-40 32-59l1664-960q15-9 32-9 20 0 36 11z" />
             </svg>
-            Contact
+            Bel ons
           </Button>
         </Link>
    <br/>
   <br/>
+   <br/>
+   <br/>
   <p> Wij bieden autocue training, coaching en begeleiden sprekers in het openbaar</p>
       </Hero>
-      <Content>
-        <SectionTitle>Latest stories</SectionTitle>
-        {posts.map(post => (
-          <Article
-            title={post.frontmatter.title}
-            date={post.frontmatter.date}
-            excerpt={post.excerpt}
-            timeToRead={post.timeToRead}
-            slug={post.fields.slug}
-            categories={post.frontmatter.categories}
-            key={post.fields.slug}
-          />
-        ))}
-      </Content>
     </Wrapper>
   </Layout>
 )

65d3c8f9b2917f600ad99b5ebd2a8ad6e7719324
Monique de Haas <monique.dondersteen@gmail.com>
2019-07-10 17:02:58 +0200

    Update index.js
    Aanpassing tekst voor opening Riny

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,11 +49,8 @@ const IndexPage = ({
   <Layout>
     <Wrapper>
       <Hero>
-        <h1>Hi.</h1>
-        <p>
-          I&apos;m John Doe, a Senior UX Developer with five years of industry experience, specializing in developing
-          React apps with the best UX users can get.
-        </p>
+        <h1>Verbeter je presentatie</h1>
+        <p>        </p>
         <Link to="/contact">
           <Button big>
             <svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
